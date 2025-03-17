da0719bf27592eb29b2930c2cf4b52a54b8d72c9
Dexter Kennedy <dexter.kennedy8@gmail.com>
2025-03-17 19:16:06 -0400

    reset base to /

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/nba_player_props_frontend/',
+  base: '/',
+  build: {
+    outDir: 'dist' // Vite's default build output folder
+  }
 })
