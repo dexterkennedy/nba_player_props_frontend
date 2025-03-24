0f988a82757985d89ee584acba49ce8e7b7de7cd
Dexter Kennedy <dexter.kennedy8@gmail.com>
2025-03-23 22:38:57 -0400

    hide tabs instead of unmounting them

--- src/App.jsx
+++ src/App.jsx
@@ -8,32 +8,28 @@ import Results from './Results';
 function App() {
   const [activeTab, setActiveTab] = useState('Generate Bets');
   const [results, setResults] = useState([]);
-  const [lifetimeStats, setLifetimeStats] = useState({})
+  const [lifetimeStats, setLifetimeStats] = useState({});
   const [bets, setBets] = useState([]);
-
-  const renderContent = () => {
-    switch (activeTab) {
-      case 'Generate Bets':
-        return <GenerateBets bets={bets} setBets={setBets} />;
-      case 'Results':
-        return <Results
-          results={results}
-          setResults={setResults}
-          lifetimeStats={lifetimeStats}
-          setLifetimeStats={setLifetimeStats} />;
-      case 'About':
-        return <About />;
-      default:
-        return <div>Select a tab</div>;
-    }
-  };
 
   return (
     <div className="App">
       <NavBar activeTab={activeTab} setActiveTab={setActiveTab} />
 
       <main>
-        {renderContent()}
+        <div style={{ display: activeTab === 'Generate Bets' ? 'block' : 'none' }}>
+          <GenerateBets bets={bets} setBets={setBets} />
+        </div>
+        <div style={{ display: activeTab === 'Results' ? 'block' : 'none' }}>
+          <Results
+            results={results}
+            setResults={setResults}
+            lifetimeStats={lifetimeStats}
+            setLifetimeStats={setLifetimeStats}
+          />
+        </div>
+        <div style={{ display: activeTab === 'About' ? 'block' : 'none' }}>
+          <About />
+        </div>
       </main>
     </div>
   );
